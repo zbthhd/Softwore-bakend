4b06af25ebe72ac7fea4eed52f1e9f5ed9e54d10
郑博天 <2248384953@qq.com>
2025-01-08 17:36:28 +0800

    建立spring结构

---
+++ src/main/java/com/example/management_platform/ManagementPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.management_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/management_platform/ManagementPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.management_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
