5f6916e1b82675ef8208a26a2f5f404721777ad4
RenLi <977347602@qq.com>
2025-01-16 18:30:46 +0800

    搞不懂啊

--- src/main/java/com/example/management_platform/mapper/StudentMapper.java
+++ src/main/java/com/example/management_platform/mapper/StudentMapper.java
@@ -1,7 +1,12 @@
 package com.example.management_platform.mapper;
 
+import com.example.management_platform.entity.Student;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface StudentMapper {
+
+    public Student find(Student student);
+
+    public void add(Student student);
 }
--- src/main/java/com/example/management_platform/service/StudentService.java
+++ src/main/java/com/example/management_platform/service/StudentService.java
@@ -1,4 +1,8 @@
 package com.example.management_platform.service;
 
+import com.example.management_platform.common.R;
+import com.example.management_platform.dto.StudentDto;
+
 public interface StudentService {
+    R register(StudentDto adminDto);
 }
